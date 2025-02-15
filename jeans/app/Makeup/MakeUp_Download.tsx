02a32946614f3bd6dbe149b01096f071f60ebe87
junyonglee2 <junyong990927@gmail.com>
2025-02-15 13:29:30 +0900

    ui: 마이페이지

---
+++ jeans/app/Makeup/MakeUp_Download.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import LottieView from 'lottie-react-native';
+import TopNavBar from '../../components/TopNavBar';
+import BottomNavBar from '../../components/BottomNavBar';
+import FullButton from '../../components/FullButton'; 
+
+export default function CompleteSignupScreen() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <TopNavBar />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>사진 저장이 완료되었습니다!</Text>
+
+        {/* 애니메이션 */}
+        <LottieView 
+          source={require('../../assets/animations/Animation - 1739343498719.json')} 
+          autoPlay
+          loop={false} 
+          style={styles.lottie}
+        />
+
+        {/* 확인 버튼 */}
+        <FullButton title="확 인" onPress={() => router.push('/Home/Mainpage')} />
+      </View>
+
+      <BottomNavBar />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 30,
+  },
+  lottie: {
+    width: 120,  
+    height: 120, 
+    marginBottom: 30,
+  },
+});
